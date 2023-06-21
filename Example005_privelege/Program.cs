75ac9c5efd5c30d38a4402972b29b8d2b6cb65f7
Stacy Turukin <ohnotredamn@gmail.com>
2023-06-22 02:22:39 +0300

    на сегодня хватит, закончим на привилигированных

---
+++ Example005_privelege/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Скажи своё имя:"); 
+string user_name = Console.ReadLine();
+
+if (user_name == "Мишанька")
+{
+    Console.WriteLine("Да ну??? Ты реально тот самый Мишаня?!?!?");
+}
+else 
+{
+   Console.Write("Блин, опять ты ");
+   Console.WriteLine(user_name);
+}
