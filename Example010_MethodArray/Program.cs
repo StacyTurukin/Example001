8408fbc33b7b5c467d1b012f90ea39dcc18696e3
Stacy Turukin <ohnotredamn@gmail.com>
2023-06-25 21:11:51 +0300

    Добавили задачу с поиском в массиве

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int [] array = { 1, 48, 3, 34, 5, 6, 27, 48 };
+
+int n = array.Length;
+int find = 48;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
+
+
+
